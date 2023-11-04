72b54fe3366742a6b0e57d512913aef482daecb9
Adr-oc <132016076+Adr-oc@users.noreply.github.com>
2023-11-04 03:26:36 -0600

    Laguages  parameter added to Project class

--- src/model/Project.java
+++ src/model/Project.java
@@ -7,14 +7,17 @@ public class Project {
     private String email;
     private String author;
     private String devs;
+    private String languages; 
 
-    public Project(String idProject, String projectName, String description, String email, String author, String devs) {
+
+    public Project(String idProject, String projectName, String description, String email, String author, String devs, String languages) {
         this.idProject = idProject;
         this.projectName = projectName;
         this.description = description;
         this.email = email;
         this.author = author;
         this.devs = devs;
+        this.languages = languages;
     }
 
     public String getIdProject() {
@@ -40,4 +43,14 @@ public class Project {
     public String getDevs() {
         return devs;
     }
+    
+    public String getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String languages) {
+        this.languages = languages;
+    }
+
+
 }
