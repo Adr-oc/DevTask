7bb0963e071962121a940442284e451f56a469d9
juampam <jpablomuralles@gmail.com>
2023-11-15 23:56:16 -0600

    debug project controller

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-import controller.Controller;
-import controller.ProjectController;
+/*import controller.Controller;
+import controller.ProjectController;*/
 public class Main {
     public static void main(String[] args) {
         //Dashboard dashboard = new Dashboard();
         //dashboard.GUI();
-        Controller.start();
+       // Controller.start();
         //ProjectController projectController = new ProjectController();
         //projectController.newProject("1", "Project 1", "Description 1","@gmail", "Author 1", "Devs 1", "Languages 1");
         //System.out.println(projectController.findProjectById("1"));
