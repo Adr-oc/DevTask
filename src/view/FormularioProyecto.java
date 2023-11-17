44fb28d42c22e405e56836edb08d1af5a67ba71f
Adr-oc <132016076+Adr-oc@users.noreply.github.com>
2023-11-17 04:51:14 -0600

    Delete src/view directory

--- src/view/FormularioProyecto.java
+++
@@ -1,8 +0,0 @@
-package view;
-import javax.swing.*;
-import javax.swing.border.Border;
-import javax.swing.border.MatteBorder;
-import controller.ProjectController;
-import java.awt.*;
-import java.io.IOException;
-
